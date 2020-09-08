34e51a2624dc6f1f9683e54b9958a9e953439249
赵凯朝 <1124918311@qq.com>
2020-09-08 19:44:58 +0800

    第三次提交

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="app">
-    app
+    <h2>父组件</h2>
+    <p>子向父发送的数据：{{val}}</p>
+    <list :msg='msg' @dataList='parentList'></list>
+    
   </div>
 </template>
 
 <script>
-
+import list from './components/list'
 export default {
   name: 'App',
   components: {
-  }
+    list
+  },
+  data() {
+    return {
+      msg:'我的父组件的',
+      val:''
+    }
+  },
+  methods: {
+    parentList(data){
+      this.val=data
+    }
+  },
 }
 </script>
 
---
+++ src/components/list.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="list">
+      <h2>子组件</h2>
+      <p>{{msg}}</p>
+      <input type="text" v-model="val">
+      <button @click='childPass'>发送数据</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'list',
+    data(){
+        return {
+            val:''
+        }
+    },
+    props:{
+        msg:{
+            type:String,
+            default:'默认值'
+        }
+    },
+    methods: {
+        childPass(){
+            this.$emit('dataList',this.val)
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
